bd711a154b567a709d78eb8e6274caa705af056e
Bruce Ediger <bediger8@gmail.com>
2021-04-04 21:21:37 -0600

    Set a bunch of strings off cmd line

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	lru "lru/lrucache"
+	"os"
 )
 
 func main() {
 	cache := lru.NewCache(20)
+	for _, str := range os.Args[1:] {
+		datum := lru.NewStringData(str)
+		cache.Set(datum, str)
+	}
 	fmt.Printf("%+v\n", cache)
 }
