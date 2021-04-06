423d217099e4dca4292262d0928c02c32730a215
Bruce Ediger <bediger8@gmail.com>
2021-04-06 07:18:44 -0600

    Look up all data in cache

--- main.go
+++ main.go
@@ -7,18 +7,27 @@ import (
 )
 
 func main() {
-	cache := lru.NewCache(20)
+	cacheSize := 5
+	cache := lru.NewCache(cacheSize)
 	unique := 0
 	for _, str := range os.Args[1:] {
 		datum := lru.NewStringData(str)
 		fmt.Printf("New datum %q\n", str)
 		if cache.Set(datum, str) {
 			unique++
-			if unique >= 20 {
+			if unique >= cacheSize {
 				cache.PrintUse()
 			}
 		}
 	}
 	cache.PrintUse()
 	fmt.Printf("Inserted %d unique items into cache\n", unique)
+	for _, str := range os.Args[1:] {
+		datum := lru.NewStringData(str)
+		if d := cache.Get(datum); d != nil {
+			fmt.Printf("Found %v in cache\n", d)
+		} else {
+			fmt.Printf("did not find %q in cache\n", str)
+		}
+	}
 }
